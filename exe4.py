b504095ad277708a62a0b214a35057d746b22073
shoams12 <shoam12326@gmail.com>
2026-01-17 23:01:05 +0200

    Add files via upload

---
+++ exe4.py
@@ -0,0 +1,4 @@
+#כתוב תוכנית בפייתון המדפיסה את השם שלך
+
+name = input("what is your name?")
+print(name)
---
+++ exe5.py
@@ -0,0 +1,11 @@
+#כתוב תוכנית בפייתון המציבה בתא זיכרון width את הערך 30
+# ובתא זיכרון height את הערך 40
+# אלו האורך והרוחב של מלבן
+# חשב את שטח המלבן לתוך תא זיכרון area
+# חשב את היקף המלבן לתוך תא זיכרון perimeter
+# הדפס את השטח וההיקף שחישבת
+width : int = 30
+height : int = 40
+area : int = width * height
+perimeter : int = width * 2 + height * 2
+print("The area:", area ," The perimeter:", perimeter)
